46f571c188f27df07c02e489d8e133a1be4e54d8
Sanmitr564 <generic39542@gmail.com>
2024-10-05 17:21:07 -0700

    Added identifier class

---
+++ Identifier.java
@@ -0,0 +1,19 @@
+public class Identifier{
+    public String identifier;
+
+    public Identifier(String identifier){
+        this.identifier = identifier;
+    }
+
+    public String getIdentifier(){ return this.identifier; }
+
+    @Override
+    public String toString(){
+        return this.identifier;
+    }
+
+    @Override
+    public boolean equals(Object s){
+        return this.identifier.equals(s.toString());
+    }
+}
