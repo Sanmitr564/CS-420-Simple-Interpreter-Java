927df4d6b766bee4f0dc7c05b51c6b5ecc733379
Sanmitr564 <generic39542@gmail.com>
2024-10-09 21:45:41 -0700

    put ELIF and ELSE instead of IF_FAILURE

--- IdentifierType.java
+++ IdentifierType.java
@@ -1,10 +1,25 @@
+import java.util.EnumSet;
+
 public enum IdentifierType {
     TYPE_DECLARATION,
     VAR,
     METHOD,
     IF,
-    IF_FAILURE,
+    ELIF,
+    ELSE,
     FOR,
     WHILE,
-    BOOLEAN
+    BOOLEAN;
+
+    public static final EnumSet<IdentifierType> ifTypes = EnumSet.of(IF, ELIF, ELSE);
+
+    public static final EnumSet<IdentifierType> ifSuccessors = EnumSet.of(ELIF, ELSE);
+
+    public boolean isIfType(){
+        return ifTypes.contains(this);
+    }
+
+    public boolean isIfSuccessor(){
+        return ifSuccessors.contains(this);
+    }
 }
